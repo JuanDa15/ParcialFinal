23770f9524dc27d74626d6d75dc797c7f7424e5e
JuanDa15 <59321452+JuanDa15@users.noreply.github.com>
2020-06-15 20:28:51 -0500

    Primeros 3cuartos nivel 1

--- CRUD/Constants.py
+++ CRUD/Constants.py
@@ -232,6 +232,7 @@ DiamondsPosC = MapInfo['layers'][14]['objects']
 ApplesPosC = MapInfo['layers'][15]['objects']
 PlatformsC= MapInfo['layers'][17]['objects']
 CannonsPosC = MapInfo['layers'][16]['objects']
+Enemys1c = MapInfo['layers'][18]['objects']
 
 #Creacion de las monedas
 for i in range(len(CoinsPosC)):
