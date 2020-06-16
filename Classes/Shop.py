f61a069b4fd0b24c2bfaba8150309d3ec61ecc63
Gandhi00 <61157514+Gandhi00@users.noreply.github.com>
2020-06-15 23:47:05 -0500

    new shop

--- Classes/Shop.py
+++ Classes/Shop.py
@@ -7,7 +7,8 @@ import datetime
 class Shop(pygame.sprite.Sprite):
     def __init__(self,position,player,prices):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Assets\Images\Sprites\Collectables\Shop.png')
+        self.imagen = pygame.image.load('Assets\Images\Sprites\Collectables\Shop.png')
+        self.image = self.imagen.subsurface(1,0,127,80)
         self.rect = self.image.get_rect()
         self.rect.x = position[0]
         self.rect.y = position[1]
@@ -21,5 +22,18 @@ class Shop(pygame.sprite.Sprite):
         self.precioGapple = prices[2]
         self.Gapple = True
 
+        #Animacion
+        self.frame = 0
+        self.espera = 2
+        self.recortes = (1,0,127,80,128,0,126,80,255,0,126,80,382,0,126,80,509,0,126,80)
+
     def update(self):
-        pass
+        if self.frame < len(self.recortes) - 1:
+            self.image = self.imagen.subsurface(self.recortes[self.frame],self.recortes[self.frame + 1],self.recortes[self.frame + 2],self.recortes[self.frame + 3])
+            if self.espera == 0: 
+                self.frame += 4
+                self.espera = 4
+            else:
+                self.espera -= 1
+        else:
+            self.frame = 0 
