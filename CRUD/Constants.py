25d83d94f07551fdb0b413f33080d97dea70d76f
JuanDa15 <59321452+JuanDa15@users.noreply.github.com>
2020-06-15 20:20:53 -0500

    distancias

--- CRUD/Constants.py
+++ CRUD/Constants.py
@@ -196,6 +196,7 @@ ApplesPosB = MapInfo['layers'][13]['objects']
 DiamondsPosB = MapInfo['layers'][14]['objects']
 CannonsPosB= MapInfo['layers'][15]['objects']
 SpikesPosB= MapInfo['layers'][16]['objects']
+Enemys1B = MapInfo['layers'][17]['objects']
 
 #Creacion de las monedas
 for i in range(len(CoinsPosB)):
