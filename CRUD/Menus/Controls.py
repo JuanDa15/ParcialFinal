2b56983d342d5befb1d599e214eb0353b61a7621
JuanDa15 <59321452+JuanDa15@users.noreply.github.com>
2020-06-17 06:45:22 -0500

    Cosas

--- CRUD/Menus/Controls.py
+++ CRUD/Menus/Controls.py
@@ -5,7 +5,7 @@ import pygame
 from CRUD import Functions
 from CRUD import UploadedFiles as UF
 from CRUD import SoundModule as SM
-from CRUD.Menus import MainMenu
+from CRUD.Menus import MainMenu as MM
 from CRUD import Constants as C
 
 def ControlsScreen(Screen):
@@ -15,12 +15,12 @@ def ControlsScreen(Screen):
     running = True
     while running:
         #BUTTONS COORDS
-        BackButton = pygame.Rect(55,530,100,50)
+        Back = pygame.Rect(55,530,100,50)
         #LOAD ELEMENTS
         Screen.fill(Functions.SelectColor('Black'))
         Functions.MakeImage(0,0,Screen,UF.getArchive('background2'))
         Functions.MakeImage(50,25,Screen,UF.getArchive('Container'))
-        Functions.MakeImage(110,610,Screen,UF.getArchive('BackImage'))
+        Functions.MakeImage(55,530,Screen,UF.getArchive('BackImage'))
         Functions.draw_text('CONTROLS',UF.TittleFont(25),[63,56,81],Screen,249.5,40)
         Functions.MakeImage(75,110,Screen,C.Left_Arrow)
         Functions.draw_text('WALK LEFT',UF.TextFont(20),[63,56,81],Screen,149,128)
@@ -31,13 +31,19 @@ def ControlsScreen(Screen):
         Functions.MakeImage(75,320,Screen,C.Space)
         Functions.draw_text('JUMP',UF.TextFont(20),[255,255,255],Screen,205,335)
         Functions.MakeImage(75,390,Screen,C.Q_Key)
+        Functions.draw_text('EAT APPLE',UF.TextFont(20),[63,56,81],Screen,149,408)
+        Functions.MakeImage(75,460,Screen,C.W_Key)
+        Functions.draw_text('HIT',UF.TextFont(20),[63,56,81],Screen,149,478)
+        Functions.draw_text('INTERACTION',UF.TextFont(20),[63,56,81],Screen,499,128)
+        Functions.MakeImage(420,110,Screen,C.E_Key)
+        
         #get mouse position--------------
         [mouse_x , mouse_y] = pygame.mouse.get_pos()
         
-        if BackButton.collidepoint(mouse_x,mouse_y):
-            Functions.MakeImage(110,610,Screen,UF.getArchive('BackImageSelected'))
+        if Back.collidepoint(mouse_x,mouse_y):
+            Functions.MakeImage(55,530,Screen,UF.getArchive('BackImageSelected'))
             if Click:
-                MainMenu.Main_Menu()
+                MM.Main_Menu(Screen)
                 
         Sprites = [C.Sound_Off_2,C.Sound_On_2,C.Sound_Up_2,C.Sound_Dowm_2,]
         SelectedSprites = [C.Sound_Off_Selected_2,C.Sound_On_Selected_2,C.Sound_Up_Selected_2,C.Sound_Down_Selected_2]
--- CRUD/Menus/LostScreen.py
+++ CRUD/Menus/LostScreen.py
@@ -6,11 +6,14 @@ from CRUD import Functions
 from CRUD.Menus import MainMenu as MM
 from CRUD import UploadedFiles as UF
 from CRUD import SoundModule as SM
+from CRUD import Constants as C
 
 def lostMenu(Screen):
     #music upload
+    pygame.mixer.init()
     pygame.mixer.music.load("Assets\Sounds\Game Over II.ogg")
     pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(0.001)
     #Definition of variables-----------------
     Click = False
     while True:
@@ -19,8 +22,12 @@ def lostMenu(Screen):
         StarAgain = pygame.Rect(300,310,200,50)
         #Draw in screen
         Screen.fill(Functions.SelectColor('Black'))
+        Functions.MakeImage(0,0,Screen,UF.getArchive('background2'))
         Functions.MakeImage(300,250,Screen,UF.getArchive('ButtonImage'))
         Functions.MakeImage(300,310,Screen,UF.getArchive('ButtonImage'))
+        Functions.MakeImage(300,450,Screen,C.KilledKing)
+        Functions.MakeImage(390,380,Screen,C.KingPig)
+        Functions.MakeImage(150,350,Screen,C.Minotaur)
         #get mouse position--------------
         [mouse_x , mouse_y] = pygame.mouse.get_pos()
         
@@ -34,8 +41,9 @@ def lostMenu(Screen):
                 Click = False
                 return 2
                 
-        Click = SM.VolumeModule(Click,Screen)
-
+        Sprites = [UF.getArchive('SoundOffImg'),UF.getArchive('SoundOnImg'),UF.getArchive('SoundUpImg'),UF.getArchive('SoundDownImg')]
+        SpritesSelected = [UF.getArchive('SoundOffSelectedImg'),UF.getArchive('SoundOnSelectedImg'),UF.getArchive('SoundUpSelectedImg'),UF.getArchive('SoundDownSelectedImg')]
+        Click = SM.VolumeModule(Click,Screen,Sprites,SpritesSelected)
         #event managment
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -49,8 +57,8 @@ def lostMenu(Screen):
                 if event.button == 1:
                     Click = True
         #update
-        Functions.draw_text('YOU DIE',UF.TittleFont(70),Functions.SelectColor('White'),Screen,89,50)
-        Functions.draw_text('MAIN MENU',UF.getArchive('ButtonFont'),Functions.SelectColor('White'),Screen,309,258.5)
-        Functions.draw_text('RESTART',UF.getArchive('ButtonFont'),Functions.SelectColor('White'),Screen,332,318.5)
+        Functions.draw_text('YOU DIE',UF.TittleFont(70),[63,56,81],Screen,89,50)
+        Functions.draw_text('MAIN MENU',UF.getArchive('ButtonFont'),[63,56,81],Screen,309,258.5)
+        Functions.draw_text('RESTART',UF.getArchive('ButtonFont'),[63,56,81],Screen,332,318.5)
         pygame.display.update()
         pygame.display.flip()
